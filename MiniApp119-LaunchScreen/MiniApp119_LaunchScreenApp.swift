04c204c0930794309b2dae48be064b4703bd2c64
Kota <maedakoutaa@gmail.com>
2023-05-16 09:32:04 +0900

    Initial Commit

---
+++ MiniApp119-LaunchScreen/MiniApp119_LaunchScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp119_LaunchScreenApp.swift
+//  MiniApp119-LaunchScreen
+//
+//  Created by 前田航汰 on 2023/05/16.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp119_LaunchScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
